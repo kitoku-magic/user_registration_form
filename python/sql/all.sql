8307141a91a6c990e9c2787dc25bb93fd01aea85
kitoku_magician <adihcu0@gmail.com>
2019-12-10 23:40:17 +0900

    テーブル定義を見直し

--- python/sql/all.sql
+++ python/sql/all.sql
@@ -1,4 +1,4 @@
-drop database if exists `user_registration_form_python`;
+DROP DATABASE IF EXISTS `user_registration_form_python`;
 
 CREATE DATABASE `user_registration_form_python` CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
@@ -7,6 +7,6 @@ use `user_registration_form_python`;
 -- mysql -uroot -p < sql/all.sqlで実行している前提
 source sql/ddl.sql
 
-source sql/dml.sql
+-- source sql/dml.sql
 
-source sql/load_data_local_infile_ken_all_csv.sql
+-- source sql/load_data_local_infile_ken_all_csv.sql
--- python/sql/ddl.sql
+++ python/sql/ddl.sql
@@ -1,119 +1,160 @@
 use `user_registration_form_python`;
 
 
--- varbinary型で、全角文字が入りそうな列は、「許容文字数 * 4」の桁数にしています
-drop table if exists `zip_address`;
+-- varbinary型で、全角文字が入りそうな列は、
+-- マスターテーブルの一部カラムを除き「許容文字数 * 4（絵文字を考慮）」の桁数にしています
+DROP TABLE IF EXISTS `prefectures`;
 
-CREATE TABLE `zip_address` (
-  `zip_address_id` mediumint unsigned NOT NULL AUTO_INCREMENT comment '郵便番号住所ID',
-  `zip_code` varbinary(7) DEFAULT '' NOT NULL comment '郵便番号',
-  `prefectures_id` tinyint unsigned DEFAULT 0 NOT NULL comment '都道府県ID',
-  `city_district_county` varbinary(64) DEFAULT '' NOT NULL comment '市区群',
-  `town_village_address` varbinary(128) DEFAULT '' NOT NULL comment '町村番地',
-  PRIMARY KEY(`zip_address_id`)
-) ENGINE=MyISAM comment='郵便番号住所';
-
-CREATE INDEX `idx_zip_address_zip_code` ON `zip_address` (`zip_code`);
-
-
-drop table if exists `single_select_category`;
-
-create table `single_select_category` (
-  `single_select_category_id` mediumint unsigned NOT NULL AUTO_INCREMENT comment '単一選択項目カテゴリーID',
-  `name` varbinary(64) DEFAULT '' NOT NULL comment '単一選択項目カテゴリー名',
-  `created_at` bigint unsigned DEFAULT 0 NOT NULL comment '作成日時のタイムスタンプ',
-  `updated_at` bigint unsigned DEFAULT 0 NOT NULL comment '更新日時のタイムスタンプ',
-  PRIMARY KEY(`single_select_category_id`)
-) ENGINE=InnoDB comment='単一選択項目カテゴリーマスタ';
+CREATE TABLE `prefectures` (
+  `prefecture_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '都道府県ID',
+  `prefecture_name` varbinary(12) NOT NULL DEFAULT '' COMMENT '都道府県名',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`prefecture_id`),
+  UNIQUE KEY `prefectures_prefecture_name_unique` (`prefecture_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='都道府県マスタ';
 
 
-drop table if exists `single_select`;
+DROP TABLE IF EXISTS `zip_addresses`;
 
-create table `single_select` (
-  `single_select_id` mediumint unsigned NOT NULL AUTO_INCREMENT comment '単一選択項目ID',
-  `single_select_category_id` mediumint unsigned DEFAULT 0 NOT NULL comment '単一選択項目カテゴリーID',
-  `name` varbinary(64) DEFAULT '' NOT NULL comment '単一選択項目名',
-  `value` varbinary(64) DEFAULT '' NOT NULL comment '単一選択項目値',
-  `created_at` bigint unsigned DEFAULT 0 NOT NULL comment '作成日時のタイムスタンプ',
-  `updated_at` bigint unsigned DEFAULT 0 NOT NULL comment '更新日時のタイムスタンプ',
-  PRIMARY KEY(`single_select_id`),
-  CONSTRAINT fk_single_select_single_select_category_id FOREIGN KEY (single_select_category_id)
-  REFERENCES single_select_category (single_select_category_id)
-  ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB comment='単一選択項目マスタ';
-
-
-drop table if exists `multiple_select_category`;
-
-create table `multiple_select_category` (
-  `multiple_select_category_id` mediumint unsigned NOT NULL AUTO_INCREMENT comment '複数選択項目カテゴリーID',
-  `name` varbinary(64) DEFAULT '' NOT NULL comment '複数選択項目カテゴリー名',
-  `created_at` bigint unsigned DEFAULT 0 NOT NULL comment '作成日時のタイムスタンプ',
-  `updated_at` bigint unsigned DEFAULT 0 NOT NULL comment '更新日時のタイムスタンプ',
-  PRIMARY KEY(`multiple_select_category_id`)
-) ENGINE=InnoDB comment='複数選択項目カテゴリーマスタ';
+CREATE TABLE `zip_addresses` (
+  `zip_address_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT '郵便番号住所ID',
+  `zip_code` varbinary(7) NOT NULL DEFAULT '' COMMENT '郵便番号',
+  `prefecture_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '都道府県ID',
+  `city_district_county` varbinary(64) NOT NULL DEFAULT '' COMMENT '市区群',
+  `town_village_address` varbinary(128) NOT NULL DEFAULT '' COMMENT '町村番地',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`zip_address_id`),
+  KEY `zip_addresses_prefecture_id_foreign` (`prefecture_id`),
+  KEY `zip_addresses_zip_code_index` (`zip_code`),
+  CONSTRAINT `zip_addresses_prefecture_id_foreign` FOREIGN KEY (`prefecture_id`) REFERENCES `prefectures` (`prefecture_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='郵便番号住所マスタ';
 
 
-drop table if exists `multiple_select`;
+DROP TABLE IF EXISTS `contact_methods`;
 
-create table `multiple_select` (
-  `multiple_select_id` mediumint unsigned NOT NULL AUTO_INCREMENT comment '複数選択項目ID',
-  `multiple_select_category_id` mediumint unsigned DEFAULT 0 NOT NULL comment '複数選択項目カテゴリーID',
-  `name` varbinary(64) DEFAULT '' NOT NULL comment '複数選択項目名',
-  `value` varbinary(64) DEFAULT '' NOT NULL comment '複数選択項目値',
-  `created_at` bigint unsigned DEFAULT 0 NOT NULL comment '作成日時のタイムスタンプ',
-  `updated_at` bigint unsigned DEFAULT 0 NOT NULL comment '更新日時のタイムスタンプ',
-  PRIMARY KEY(`multiple_select_id`),
-  CONSTRAINT fk_multiple_select_multiple_select_category_id FOREIGN KEY (multiple_select_category_id)
-  REFERENCES multiple_select_category (multiple_select_category_id)
-  ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB comment='複数選択項目マスタ';
+CREATE TABLE `contact_methods` (
+  `contact_method_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '連絡方法ID',
+  `contact_method_name` varbinary(32) NOT NULL DEFAULT '' COMMENT '連絡方法名',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`contact_method_id`),
+  UNIQUE KEY `contact_methods_contact_method_name_unique` (`contact_method_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='連絡方法マスタ';
 
 
-drop table if exists `user`;
+DROP TABLE IF EXISTS `knew_triggers`;
 
-create table `user` (
-  `user_id` bigint unsigned NOT NULL AUTO_INCREMENT comment 'ユーザーID',
-  `mail_address` varbinary(512) DEFAULT '' NOT NULL comment 'メールアドレス',
-  `token` varbinary(128) DEFAULT '' NOT NULL comment 'トークン値',
-  `registration_status` tinyint unsigned DEFAULT 0 NOT NULL comment '登録状況',
-  `last_name` varbinary(32) DEFAULT '' NOT NULL comment '苗字',
-  `first_name` varbinary(32) DEFAULT '' NOT NULL comment '名前',
-  `last_name_hiragana` varbinary(64) DEFAULT '' NOT NULL comment '苗字（ひらがな）',
-  `first_name_hiragana` varbinary(64) DEFAULT '' NOT NULL comment '名前（ひらがな）',
-  `sex_id` tinyint unsigned DEFAULT 0 NOT NULL comment '性別ID',
-  `birth_day` date DEFAULT '0001-01-01' NOT NULL comment '誕生日',
-  `zip_code` varbinary(8) DEFAULT '' NOT NULL comment '郵便番号',
-  `prefectures_id` tinyint unsigned DEFAULT 0 NOT NULL comment '都道府県ID',
-  `city_street_address` varbinary(256) DEFAULT '' NOT NULL comment '市区町村・丁目・番地',
-  `building_room_address` varbinary(256) DEFAULT '' NOT NULL comment '建物名・室名',
-  `telephone_number` varbinary(13) DEFAULT '' NOT NULL comment '電話番号',
-  `job_id` tinyint unsigned DEFAULT 0 NOT NULL comment '職業ID',
-  `job_other` varbinary(64) DEFAULT '' NOT NULL comment '職業その他',
-  `is_latest_news_hoped` tinyint(1) unsigned DEFAULT 0 NOT NULL comment '最新情報の希望状況',
-  `file_name` varbinary(256) DEFAULT '' NOT NULL comment 'ファイル名',
-  `file_path` varbinary(512) DEFAULT '' NOT NULL comment 'ファイルパス',
-  `remarks` BLOB NOT NULL comment '備考',
-  `is_personal_information_provide_agreed` tinyint(1) unsigned DEFAULT 0 NOT NULL comment '個人情報提供の同意状況',
-  `created_at` bigint unsigned DEFAULT 0 NOT NULL comment '作成日時のタイムスタンプ',
-  `updated_at` bigint unsigned DEFAULT 0 NOT NULL comment '更新日時のタイムスタンプ',
-  PRIMARY KEY(`user_id`),
-  UNIQUE KEY `uniq_user_mail_address` (`mail_address`)
-) ENGINE=InnoDB comment='ユーザー';
+CREATE TABLE `knew_triggers` (
+  `knew_trigger_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '知ったきっかけID',
+  `knew_trigger_name` varbinary(64) NOT NULL DEFAULT '' COMMENT '知ったきっかけ名',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`knew_trigger_id`),
+  UNIQUE KEY `knew_triggers_knew_trigger_name_unique` (`knew_trigger_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='知ったきっかけマスタ';
 
 
-drop table if exists `user_multiple_select`;
+DROP TABLE IF EXISTS `birth_days`;
 
-create table `user_multiple_select` (
-  `user_id` bigint unsigned DEFAULT 0 NOT NULL comment 'ユーザーID',
-  `multiple_select_id` mediumint unsigned DEFAULT 0 NOT NULL comment '複数選択項目ID',
-  `created_at` bigint unsigned DEFAULT 0 NOT NULL comment '作成日時のタイムスタンプ',
-  `updated_at` bigint unsigned DEFAULT 0 NOT NULL comment '更新日時のタイムスタンプ',
-  PRIMARY KEY(`user_id`, `multiple_select_id`),
-  CONSTRAINT fk_user_multiple_select_user_id FOREIGN KEY (user_id)
-  REFERENCES `user` (user_id)
-  ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT fk_user_multiple_select_multiple_select_id FOREIGN KEY (multiple_select_id)
-  REFERENCES `multiple_select` (multiple_select_id)
-  ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB comment='ユーザー複数選択項目';
+CREATE TABLE `birth_days` (
+  `birth_day_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '誕生日ID',
+  `birth_day` date NOT NULL DEFAULT '0001-01-01' COMMENT '誕生日',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`birth_day_id`),
+  UNIQUE KEY `birth_days_birth_day_unique` (`birth_day`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='誕生日マスタ';
+
+
+DROP TABLE IF EXISTS `jobs`;
+
+CREATE TABLE `jobs` (
+  `job_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '職業ID',
+  `job_name` varbinary(32) NOT NULL DEFAULT '' COMMENT '職業名',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`job_id`),
+  UNIQUE KEY `jobs_job_name_unique` (`job_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='職業マスタ';
+
+
+DROP TABLE IF EXISTS `sexes`;
+
+CREATE TABLE `sexes` (
+  `sex_id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT COMMENT '性別ID',
+  `sex_name` varbinary(32) NOT NULL DEFAULT '' COMMENT '性別名',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`sex_id`),
+  UNIQUE KEY `sexes_sex_name_unique` (`sex_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='性別マスタ';
+
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ユーザーID',
+  `mail_address` varbinary(512) NOT NULL DEFAULT '' COMMENT 'メールアドレス',
+  `token` varbinary(128) NOT NULL DEFAULT '' COMMENT 'トークン値',
+  `registration_status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '登録状況',
+  `last_name` varbinary(32) NOT NULL DEFAULT '' COMMENT '苗字',
+  `first_name` varbinary(32) NOT NULL DEFAULT '' COMMENT '名前',
+  `last_name_hiragana` varbinary(64) NOT NULL DEFAULT '' COMMENT '苗字（ひらがな）',
+  `first_name_hiragana` varbinary(64) NOT NULL DEFAULT '' COMMENT '名前（ひらがな）',
+  `sex_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '性別ID',
+  `birth_day_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '誕生日ID',
+  `zip_code` varbinary(8) NOT NULL DEFAULT '' COMMENT '郵便番号',
+  `prefecture_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '都道府県ID',
+  `city_street_address` varbinary(256) NOT NULL DEFAULT '' COMMENT '市区町村・丁目・番地',
+  `building_room_address` varbinary(128) NOT NULL DEFAULT '' COMMENT '建物名・室名',
+  `telephone_number` varbinary(13) NOT NULL DEFAULT '' COMMENT '電話番号',
+  `job_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '職業ID',
+  `job_other` varbinary(64) NOT NULL DEFAULT '' COMMENT '職業その他',
+  `is_latest_news_hoped` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '最新情報の希望状況',
+  `file_name` varbinary(256) NOT NULL DEFAULT '' COMMENT 'ファイル名',
+  `file_path` varbinary(512) NOT NULL DEFAULT '' COMMENT 'ファイルパス',
+  `remarks` blob NOT NULL COMMENT '備考',
+  `is_personal_information_provide_agreed` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '個人情報提供の同意状況',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `users_mail_address_unique` (`mail_address`),
+  KEY `users_sex_id_foreign` (`sex_id`),
+  KEY `users_birth_day_id_foreign` (`birth_day_id`),
+  KEY `users_prefecture_id_foreign` (`prefecture_id`),
+  KEY `users_job_id_foreign` (`job_id`),
+  CONSTRAINT `users_birth_day_id_foreign` FOREIGN KEY (`birth_day_id`) REFERENCES `birth_days` (`birth_day_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users_job_id_foreign` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users_prefecture_id_foreign` FOREIGN KEY (`prefecture_id`) REFERENCES `prefectures` (`prefecture_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `users_sex_id_foreign` FOREIGN KEY (`sex_id`) REFERENCES `sexes` (`sex_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ユーザー';
+
+
+DROP TABLE IF EXISTS `user_contact_methods`;
+
+CREATE TABLE `user_contact_methods` (
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'ユーザーID',
+  `contact_method_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '連絡方法ID',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`user_id`,`contact_method_id`),
+  KEY `user_contact_methods_contact_method_id_foreign` (`contact_method_id`),
+  CONSTRAINT `user_contact_methods_contact_method_id_foreign` FOREIGN KEY (`contact_method_id`) REFERENCES `contact_methods` (`contact_method_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_contact_methods_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ユーザー連絡方法';
+
+
+DROP TABLE IF EXISTS `user_knew_triggers`;
+
+CREATE TABLE `user_knew_triggers` (
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'ユーザーID',
+  `knew_trigger_id` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '知ったきっかけID',
+  `created_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '作成日時のタイムスタンプ',
+  `updated_at` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT '更新日時のタイムスタンプ',
+  PRIMARY KEY (`user_id`,`knew_trigger_id`),
+  KEY `user_knew_triggers_knew_trigger_id_foreign` (`knew_trigger_id`),
+  CONSTRAINT `user_knew_triggers_knew_trigger_id_foreign` FOREIGN KEY (`knew_trigger_id`) REFERENCES `knew_triggers` (`knew_trigger_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_knew_triggers_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ユーザー知ったきっかけ';
